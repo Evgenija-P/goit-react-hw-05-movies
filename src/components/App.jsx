89c19095ccc9fad868a711c8adcb3be5fa9a90f7
Evgenija-P <99011054+Evgenija-P@users.noreply.github.com>
2022-10-18 16:37:09 +0300

    pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+import { Routes, Route } from 'react-router-dom';
+import { Home } from '../pages/Home.';
+import { Movies } from '../components/Movies/Movies';
+import { MovieDetails } from '../components/MovieDetails/MovieDetails';
+import { Cast } from '../pages/Cast';
+import { Reviews } from '../components/Reviews/Reviews';
+import { NotFound } from '../pages/NotFound';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route index element={<div>Homepage</div>} />
+          <Route path="movies" element={<Movies />}>
+            <Route index element={<div>Movies</div>} />
+            <Route path=":movieDetails" element={<MovieDetails />}>
+              <Route path="cast" element={<Cast />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
+          </Route>
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
---
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -0,0 +1,7 @@
+export const MovieDetails = () => {
+  return (
+    <div>
+      <p>MovieDetails</p>
+    </div>
+  );
+};
