eb98d850c16a18524f72a733113f00e4985e8910
Evgenija-P <99011054+Evgenija-P@users.noreply.github.com>
2022-10-20 18:04:36 +0000

    Deploying to gh-pages from @ Evgenija-P/goit-react-hw-05-movies@5055e7b1851667e67d163bc4fe55e85c97d78bd0 🚀

---
+++ static/js/820.07d4d87a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[820],{8820:function(t,c,e){e.d(c,{bzc:function(){return i}});var a=e(9983);function i(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 1024 1024"},child:[{tag:"path",attr:{d:"M912 190h-69.9c-9.8 0-19.1 4.5-25.1 12.2L404.7 724.5 207 474a32 32 0 0 0-25.1-12.2H112c-6.7 0-10.4 7.7-6.3 12.9l273.9 347c12.8 16.2 37.4 16.2 50.3 0l488.4-618.9c4.1-5.1.4-12.8-6.3-12.8z"}}]})(t)}}}]);
+//# sourceMappingURL=820.07d4d87a.chunk.js.map
